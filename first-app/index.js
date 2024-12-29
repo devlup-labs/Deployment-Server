09db5493c361dc886461b8eca05a4ea817b6c33f
Mohi1038 <b23cs1038@iitj.ac.in>
2024-12-29 23:22:57 +0530

    Update index.js

--- first-app/index.js
+++ first-app/index.js
@@ -3,28 +3,24 @@
  * @param {import('probot').Probot} app
  */
 export default (app) => {
-  // Log when the app is loaded
   app.log.info("Yay, the app was loaded!");
 
-  // Listen for the event when an issue is opened
-  app.on("issues.opened", async (context) => {
-    // Get the username of the user who opened the issue
-    const userName = context.payload.sender.login;
-    // Get the name of the repository where the issue was opened
-    const repoName = context.payload.repository.name;
+  app.on("installation_repositories", async (context) => {
+    const { action, repositories_added, repositories_removed, sender } =
+      context.payload;
 
-    // Create a message with the user's name and repository name
-    const issueComment = context.issue({
-      body: `Thanks for opening the issue @${userName}! You opened it in the repository: ${repoName}.`,
-    });
+    const userName = sender?.login || "unknown user";
 
-    // Post the comment on the issue
-    return context.octokit.issues.createComment(issueComment);
+    if (action === "added" && repositories_added.length > 0) {
+      repositories_added.forEach((repo) => {
+        app.log.info(`Added: ${userName}, ${repo.name}`);
+      });
+    }
+
+    if (action === "removed" && repositories_removed.length > 0) {
+      repositories_removed.forEach((repo) => {
+        app.log.info(`Removed: ${userName}, ${repo.name}`);
+      });
+    }
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
