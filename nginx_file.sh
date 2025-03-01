e3fbde0cd7580e108df902b6157f8f7e8f3adfa2
Shweta <b23cm1037@iitj.ac.in>
2025-03-01 14:10:02 +0530

    creates nginx file for frontend, backend or both as required..

--- nginx_file.sh
+++ nginx_file.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Check if running with sudo privileges
+# Check if running with sudo privileges to write in /etc/nginx/conf.d/ 
+# because it is protected and require root acces
 if [ "$EUID" -ne 0 ]; then
   echo "Please run with sudo privileges to write to /etc/nginx/conf.d/"
   exit 1
@@ -20,23 +21,32 @@ fi
 
 # Check if enough arguments are passed
 if [ "$#" -ne 8 ]; then
-    echo "Usage: $0 <file-name> <ip/domain> frontend <frontend-route> <frontend-port> backend <backend-route> <backend-port>"
+    echo "Usage: $0 <file-name> <ip/domain> <frontend: yes/no> <frontend-route> <frontend-port> <backend: yes/no> <backend-route> <backend-port>"
     exit 1
 fi
 
 # Assign input arguments
 FILE_NAME=$1
 DOMAIN=$2
+FRONTEND_PRESENT=$3
 FRONTEND_ROUTE=$4
 FRONTEND_PORT=$5
+BACKEND_PRESENT=$6
 BACKEND_ROUTE=$7
 BACKEND_PORT=$8
 
 OUTPUT_FILE="/etc/nginx/conf.d/$FILE_NAME.conf"
 
-# Validate that ports are numbers
-if ! [[ "$FRONTEND_PORT" =~ ^[0-9]+$ ]] || ! [[ "$BACKEND_PORT" =~ ^[0-9]+$ ]]; then
-    echo "Error: Ports must be numeric values."
+# Validate that ports are numbers if they are present
+# For frontend
+if [ "$FRONTEND_PRESENT" == "yes" ] && ! [[ "$FRONTEND_PORT" =~ ^[0-9]+$ ]]; then
+    echo "Error: Frontend port must be a numeric value."
+    exit 1
+fi
+
+# For backend
+if [ "$BACKEND_PRESENT" == "yes" ] && ! [[ "$BACKEND_PORT" =~ ^[0-9]+$ ]]; then
+    echo "Error: Backend port must be a numeric value."
     exit 1
 fi
 
@@ -47,22 +57,29 @@ server {
     listen [::]:80;
     server_name $DOMAIN;
     index index.html;
+EOF
 
+if [ "$FRONTEND_PRESENT" == "yes" ]; then
+    echo "
     # Frontend app
     location $FRONTEND_ROUTE {
         proxy_pass http://localhost:$FRONTEND_PORT;
         try_files \$uri \$uri/ /index.html;
-    }
+    }" >> $OUTPUT_FILE
+fi
 
+if [ "$BACKEND_PRESENT" == "yes" ]; then
+    echo "
     # Backend API requests
     location $BACKEND_ROUTE {
         proxy_pass http://localhost:$BACKEND_PORT;
         proxy_set_header Host \$host;
-    }
-}
-EOF
+    }" >> $OUTPUT_FILE
+fi
 
-# Check if the file was created successfully
+echo "}" >> $OUTPUT_FILE
+
+# Check if the file was created successfully or not
 if [ ! -f "$OUTPUT_FILE" ]; then
     echo "Failed to create the Nginx configuration file."
     exit 1
